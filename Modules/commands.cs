6ddf7ac388a163ea16eb3107420c2dba5d38e1f5
JackBarns <38585068+JackBarns@users.noreply.github.com>
2018-06-06 00:19:19 +0100

    Free Warning Bot
    Free Warning Bot

---
+++ Modules/commands.cs
@@ -0,0 +1,148 @@
+﻿using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Discord;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace new_bot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        sqlFunctionsHandler handler = new sqlFunctionsHandler();
+
+        decimal bot_version = 1.2m;
+
+        string bot_uptime = Convert.ToString(DateTime.Now - Process.GetCurrentProcess().StartTime.AddTicks(-(Process.GetCurrentProcess().StartTime.Ticks % TimeSpan.TicksPerSecond)));
+
+        [Command("!about")]
+        public async Task AboutAsync()
+        {
+            try
+            {
+                await Context.User.SendMessageAsync($":bookmark: Bot developer:{Environment.NewLine}{Environment.NewLine}:flag_gb: **{Context.Guild.GetUser(265637738370433035).Username}#{Context.Guild.GetUser(265637738370433035).Discriminator}** " + Environment.NewLine + Environment.NewLine +
+                    $"Version: **{bot_version}**" + Environment.NewLine +
+                    $"Language: **C#**" + Environment.NewLine +
+                    $"Uptime: **{bot_uptime}**"
+                    );
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally { await Context.Message.DeleteAsync(); }
+        }
+
+        [Command("!warns")]
+        public async Task warn(IGuildUser targetUser)
+        {
+            try
+            {
+                var builder = new EmbedBuilder()
+                    .WithTitle("User Warns")
+                    .WithDescription($"User {targetUser.Mention} has **{handler.numberOfWarns(targetUser.Id)}**/3 warns.")
+                    .WithColor(new Color(0xFF0000))
+                    .WithTimestamp(DateTime.UtcNow);
+
+                var embed = builder.Build();
+                await Context.User.SendMessageAsync("", false, embed);
+            }
+            catch (Exception)
+            {
+                throw;
+            } finally { await Context.Message.DeleteAsync(); }
+        }
+
+        [Command("!warn")]
+        public async Task warn(IGuildUser warnedUser, [Remainder] string reason)
+        {
+            try
+            {
+                if (handler.isStaff(Context.Guild.GetUser(Context.User.Id)) == false)
+                {
+                    await Context.User.SendMessageAsync(":exclamation: Insufficient permissions for this command");
+                    return;
+                }
+
+                int previousWarnsTotal = handler.numberOfWarns(Context.User.Id);
+                handler.warnUser(warnedUser.Id, Context.User.Id, reason);
+
+                var builder = new EmbedBuilder()
+                    .WithTitle("User Warned")
+                    .WithDescription($":exclamation: User {warnedUser.Mention} has just been warned by {Context.User.Mention}\t\t\t" + Environment.NewLine + $"**Reason:** {reason}" + Environment.NewLine +
+                    $"**Total Warnings:** {handler.numberOfWarns(warnedUser.Id)}/3" + Environment.NewLine + $"**Location:** #{Context.Channel}")
+                    .WithColor(new Color(0xFF0000))
+                    .WithTimestamp(DateTime.UtcNow);
+                var embed = builder.Build();
+
+                var pmBuilder = new EmbedBuilder()
+                    .WithTitle("You've been warned")
+                    .WithDescription($":exclamation: You've been warned by {Context.User.Mention}\t\t\t" + Environment.NewLine + $"**Reason:** {reason}" + Environment.NewLine +
+                    $"**Total Warnings:** {handler.numberOfWarns(warnedUser.Id)}/3" + Environment.NewLine + $"**Location:** #{Context.Channel}")
+                    .WithColor(new Color(0xFF0000))
+                    .WithTimestamp(DateTime.UtcNow);
+                var pmEmbed = pmBuilder.Build();
+
+
+                var pmSuccBuilder = new EmbedBuilder()
+                    .WithTitle("User Warned")
+                    .WithDescription($":exclamation: You've just warned {warnedUser.Mention}\t\t\t" + Environment.NewLine + $"**Reason:** {reason}" + Environment.NewLine +
+                    $"**Total Warnings:** {handler.numberOfWarns(warnedUser.Id)}/3" + Environment.NewLine + $"**Location:** #{Context.Channel}")
+                    .WithColor(new Color(0x40E0D0))
+                    .WithTimestamp(DateTime.UtcNow);
+                var pmSuccEmbed = pmSuccBuilder.Build();
+
+                await Context.User.SendMessageAsync("", false, pmSuccEmbed);
+                await Context.Guild.GetTextChannel(451423996303376384).SendMessageAsync("", false, embed);
+                await warnedUser.SendMessageAsync("", false, pmEmbed);
+
+                if (handler.numberOfWarns(warnedUser.Id) >= 3) { await Context.Guild.AddBanAsync(warnedUser, 3, "Warnings exceeded 3"); handler.removeWarns(warnedUser.Id); }
+
+            } catch (Exception)
+            {
+                throw;
+            } finally { await Context.Message.DeleteAsync(); }
+        }
+    }
+
+    public class commandExceptionHandler
+    {
+        public async Task HandleCommandException(SocketUserMessage message, IResult ex)
+        {
+            try
+            {
+                var builder = new EmbedBuilder()
+                    .WithTitle($":exclamation: Invalid Syntax - You entered something incorrectly.")
+                    //.WithDescription("A full list of updated commands and their roles.")
+                    .WithColor(new Color(0x9E2CCC))
+                    .WithTimestamp(DateTime.UtcNow)
+                    .WithFooter(footer =>
+                    {
+                        footer
+                            .WithText("Fallout 76: Helper Bot");
+                            //.WithIconUrl("https://media.discordapp.net/attachments/325286562386673665/453660638607048704/FO76_Bot_pfp.png?width=521&height=521");
+                    })
+                    .WithThumbnailUrl("https://media.discordapp.net/attachments/325286562386673665/453660638607048704/FO76_Bot_pfp.png?width=521&height=521")
+                    .WithAuthor(author =>
+                    {
+                        author
+                            .WithName("Fallout 76: Helper Bot")
+                            //.WithUrl("https://discord.gg/CJXCVaW")
+                            .WithIconUrl("https://media.discordapp.net/attachments/325286562386673665/453660638607048704/FO76_Bot_pfp.png?width=521&height=521");
+                    })
+                    .AddField("To warn a user", "``!warn <user> <reason>`` _(optional)_");
+                var embed = builder.Build();
+                await message.Author.SendMessageAsync("", false, embed);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                await message.DeleteAsync();
+            }
+        }
+    }
+}
---
+++ Modules/sqlFunctionsHandler.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Linq;
+using Discord;
+using MySql.Data.MySqlClient;
+
+namespace new_bot.Modules
+{
+    public class sqlFunctionsHandler {
+        public string Host;
+        public string Port;
+        public string Password;
+        public string Username;
+        public string Database;
+        public string IntegratedSecurity;
+
+        public ulong[] staffRoleIds = new ulong[] { 451422933634777109 , 451423817923952651, 132908718588166144, 453651587500605456 };
+
+        public static MySqlConnection connection = new MySqlConnection();
+
+        public MySqlCommand command = connection.CreateCommand();
+        public MySqlDataReader reader;
+       
+        public void Initialize() { connection.ConnectionString = $"Data Source={Host};Integrated Security={IntegratedSecurity};port={Port};username={Username};password={Password};database={Database};";  }
+
+       public void testConnection()
+        {
+            try
+            {
+                connection.Open();
+                Sql_logLine("Connection opened.");
+                Sql_logLine($"ServerVersion: {connection.ServerVersion}");
+                Sql_logLine($"State: {connection.State}");
+            }
+            catch (MySqlException)
+            {
+                throw;
+            } finally {
+                closeConnection(connection);
+            }
+        }
+
+        public void closeConnection(MySqlConnection connection)
+        {
+            if (connection.State != System.Data.ConnectionState.Closed)
+            {
+                connection.Close();
+            }
+        }
+
+        public bool isStaff(IGuildUser user)
+        {
+            foreach(ulong roleId in user.RoleIds)
+            {
+                if (staffRoleIds.Contains<ulong>(roleId))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Int32 numberOfWarns(ulong targetUserId)
+        {
+            try
+            {
+                command.CommandText = $"SELECT COUNT(warnedUser) FROM warningbot.warningdata WHERE warnedUser = '{targetUserId}';";
+                connection.Open();
+                return Convert.ToInt32(command.ExecuteScalar());
+            }
+            catch (MySqlException i)
+            {
+                Console.Write(i);
+                throw;
+            } finally { closeConnection(connection); }
+        }
+
+        public void warnUser(ulong targetUserId, ulong sourceUserId, string reason)
+        {
+            try
+            {
+                command.CommandText = $"INSERT INTO warningbot.warningdata(warnedUser, warnedBy, reason) VALUES ({targetUserId}, {sourceUserId}, '{Uri.EscapeDataString(reason)}');";
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (MySqlException)
+            {
+                throw;
+            } finally { closeConnection(connection); }
+        }
+
+        public bool IsBotChannel(ulong channelId)
+        {
+            if (channelId == 453651564079480832)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public void removeWarns(ulong targetUser)
+        {
+            try
+            {
+                command.CommandText = $"DELETE * FROM warningbot.warningdata WHERE warnedUser = '{targetUser}';";
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (MySqlException)
+            {
+                throw;
+            }
+            finally { closeConnection(connection); }
+        }
+
+        private void discord_logLine(string input) {
+            ConsoleColor currentColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss", System.Globalization.DateTimeFormatInfo.InvariantInfo)} Discord     {input}");
+            Console.ForegroundColor = currentColor;
+        }
+
+        public void IoSql_logLine(string input)
+        {
+            ConsoleColor currentColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss", System.Globalization.DateTimeFormatInfo.InvariantInfo)} SQL-IO\t     {input}");
+            Console.ForegroundColor = currentColor;
+        }
+
+        private void Sql_logLine(string input) {
+            ConsoleColor currentColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss", System.Globalization.DateTimeFormatInfo.InvariantInfo)} SQL\t     {input}");
+            Console.ForegroundColor = currentColor;
+        }
+
+        public void log_logLine(string input)
+        {
+            ConsoleColor currentColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss", System.Globalization.DateTimeFormatInfo.InvariantInfo)} LOG\t     Message object{Environment.NewLine}{input}");
+            Console.ForegroundColor = currentColor;
+        }
+    }
+    public class CPlayerData
+    {
+        public ulong mPlayerId;
+        public string mReason;
+        public DateTime mTimeStamp;
+        public string mpRepPoint;
+    }
+}
